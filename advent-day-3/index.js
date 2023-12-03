18e43271cb4ab194dadedced505c69b3b7fe5b29
Justin Edwards <JustinDEdwards@gmail.com>
2023-12-03 12:16:36 -0500

    day 3 part 2

--- advent-day-3/index.js
+++ advent-day-3/index.js
@@ -2,31 +2,27 @@ const fs = require('fs');
 
 const lines = fs.readFileSync('input', 'utf-8').split(/\r?\n/);
 
-console.log(lines)
 const symbolPositions= [];
 
 lines.forEach((v, i) => {
-  const matches = [...v.matchAll(/[^0-9.]/g)];
+  const matches = [...v.matchAll(/[*]/g)];
   matches.forEach(m => symbolPositions.push({
     row: i,
-    column: m.index
+    column: m.index,
+    parts: []
   }));
 });
 
-const total = lines.reduce((accumulator, line, currentRow) => {
+lines.forEach((line, currentRow) => {
   const numbers = [...line.matchAll(/\d+/g)];
-  let amountToAdd = 0;
-  console.log(numbers)
   numbers.forEach(n =>{
-    console.log(n[0]);
-    if(symbolPositions.find(s => s.row >= currentRow -1
-      && s.row <= currentRow + 1
-      && s.column >= n.index - 1
-      && s.column <= n.index + n[0].length)){
-      amountToAdd += Number(n[0]);
-    }
-  })
-  return accumulator + amountToAdd;
-}, 0)
+    symbolPositions.filter(s => s.row >= currentRow -1
+        && s.row <= currentRow + 1
+        && s.column >= n.index - 1
+        && s.column <= n.index + n[0].length).forEach(v => v.parts.push(n));
+  });
+});
+
+total = symbolPositions.filter(s => s.parts.length === 2).reduce((acc, gear) => gear.parts[0] * gear.parts[1] + acc, 0)
 
 console.log(total)
