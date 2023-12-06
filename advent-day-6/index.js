e84e1c11c437334c05e990c78d87f733eb875b9b
Justin Edwards <JustinDEdwards@gmail.com>
2023-12-06 01:27:33 -0500

    advent day 6 part 2

--- advent-day-6/index.js
+++ advent-day-6/index.js
@@ -2,24 +2,13 @@ const fs = require('fs');
 
 const lines = fs.readFileSync('input', 'utf-8').split(/\r?\n/);
 
-function numberStringToArray(str) {
-  return str.trim().split(/\s+/).map(Number)
+const time = Number(lines[0].replace(/[^\d]/g, ''));
+const distance = Number(lines[1].replace(/[^\d]/g, ''));
+
+let waysToBeat = 0;
+
+for(let i = 0; i < time; i++) {
+  if(i * (time - i) > distance ) waysToBeat++;
 }
 
-const times = numberStringToArray(lines[0].split(':')[1]);
-const distances = numberStringToArray(lines[1].split(':')[1]);
-
-const records = times.map((t, i) => {
-  return {
-    "time": t,
-    "distance": distances[i]
-  }
-});
-
-const waysToBeat = records.map(r =>{
-  return Array.from({length: r.time}, (_, i) => i).reduce((acc, i) => {
-    return i * (r.time - i) > r.distance ? acc + 1 : acc;
-  })
-})
-
-console.log(waysToBeat.reduce((acc, w) => acc * w, 1));
+console.log(waysToBeat);
