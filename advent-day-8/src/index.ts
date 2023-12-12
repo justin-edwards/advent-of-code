51d58a0d24693f4fde40b9935417a94430534b2d
Justin Edwards <JustinDEdwards@gmail.com>
2023-12-12 16:28:33 -0500

    advent day 8 part 2

--- advent-day-8/src/index.ts
+++ advent-day-8/src/index.ts
@@ -22,7 +22,9 @@ const parsedNodeList: { [name: string]: Node } = {};
 
 const moveList: string = lines[0];
 
-let currentNode: Node = {} as Node
+let startNodes: Node[] = [] as Node[];
+let endNodes: Node[] = [] as Node[];
+
 
 function assignChildNodeOrAddToWaitList(node: Node, childType: ChildType, name: string){
     if(parsedNodeList[name]){
@@ -53,19 +55,52 @@ for (let i = 1; i < lines.length; i++){
             entry.node.right = node;
         }
     }
-    if(name === "AAA"){
-        currentNode = node;
+    if(name.match(/..A/g)){
+        startNodes.push(node);
+    } else if (name.match(/..Z/g)){
+        endNodes.push(node);
     }
-}
-let i = 0;
-while(currentNode.name !== "ZZZ"){
-    const move = moveList[i%moveList.length];
-    if(move === "L"){
-        currentNode = currentNode.left;
-    } else {
-        currentNode = currentNode.right;
-    }
-    i++;
 }
 
-console.log(i);
+let pathLengths :number[]  = [];
+
+startNodes.forEach(n =>{
+    let i = 0;
+    let currentNode = n;
+    do{
+        const move = moveList[i%moveList.length];
+        if(move === "L"){
+            currentNode = currentNode.left;
+        } else {
+            currentNode = currentNode.right;
+        }
+        if(endNodes.find(end => end === currentNode)){
+            pathLengths.push(i + 1);
+            console.log(n.name, i + 1, (i + 1) % moveList.length, currentNode.name);
+            break;
+        }
+        i++;
+    } while (currentNode !== n || i%moveList.length === 0)
+})
+
+
+function calculateLCM (lengths : number[]) : number {
+    function getGCD(a : number, b : number) : number {
+        if (b === 0) {
+            return a;
+        }
+        return getGCD(b, a % b);
+    }
+
+    function getLCM(a : number, b : number) : number {
+        return (a * b) / getGCD(a, b);
+    }
+
+    let n = 1;
+    for(let i = 0; i < lengths.length; i++){
+        n = getLCM(n, lengths[i]);
+    }
+    return n;
+}
+
+console.log(calculateLCM(pathLengths));
